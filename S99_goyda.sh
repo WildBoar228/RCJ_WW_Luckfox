d1d2ee9676249c264da5cb731cd79c15127b1a52
WildBoar228 <igolovachenko@yandex.ru>
2026-07-25 16:56:11 +0300

    autostart (1st trial)

---
+++ S99_goyda.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DELAY=2
+
+start() {
+    export LD_LIBRARY_PATH=/oem/usr/lib:$LD_LIBRARY_PATH
+    (sleep $DELAY && echo "Start rcj_ww_luckfox" >> /var/log/rcj_init.log && /root/rcj_ww_luckfox >> /var/log/rcj_ww_luckfox.log 2>&1) &
+}
+
+case $1 in
+        start)
+                echo "start S99_goyda" >> /var/log/rcj_init.log
+                start
+                ;;
+        stop)
+                echo "stop S99_goyda" >> /var/log/rcj_init.log
+                ;;
+        *)
+                exit 1
+                ;;
+esac
