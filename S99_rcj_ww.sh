af7054acbc67de8e03c9f8749ae6014af0e053e8
WildBoar228 <igolovachenko@yandex.ru>
2026-07-28 20:29:40 +0300

    autostart script

--- S99_rcj_ww.sh
+++ S99_rcj_ww.sh
@@ -6,18 +6,18 @@ start() {
     export LD_LIBRARY_PATH=/oem/usr/lib:$LD_LIBRARY_PATH
     (
         sleep $DELAY &&
-        echo "Start rcj_ww_luckfox" >> /var/log/rcj_init.log &&
-        /root/rcj_ww_luckfox >> /var/log/rcj_ww_luckfox.log 2>&1
+        echo "Start rcj_ww_vision" >> /var/log/rcj_init.log &&
+        /root/rcj_ww_vision >> /var/log/rcj_ww_vision.log 2>&1
     ) &
 }
 
 case $1 in
         start)
-                echo "start S99_goyda" >> /var/log/rcj_init.log
+                echo "start S99_rcj_ww" >> /var/log/rcj_init.log
                 start
                 ;;
         stop)
-                echo "stop S99_goyda" >> /var/log/rcj_init.log
+                echo "stop S99_rcj_ww" >> /var/log/rcj_init.log
                 ;;
         *)
                 exit 1
